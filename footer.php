fb738008cd1be1fa44e476722438616871232cab
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-09-28 19:31:05 +0200

    added social menu to the index

--- footer.php
+++ footer.php
@@ -3,18 +3,17 @@
                 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer Widget Area") ) : ?>
                 <?php endif;?>
             </nav>
-
         </footer>
     </body> 
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177101450-1"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-    gtag('config', 'UA-177101450-1');
+        gtag('config', 'UA-177101450-1');
     </script>
 
 
--- index.php
+++ index.php
@@ -2,7 +2,8 @@
     get_header(); 
 ?>    
         <main> 
-        <?php if(is_front_page()): ?>
+        <?php if( is_front_page() ): ?>
+            <!-------------------------->
             <?php if( have_posts() ): ?>
                 <?php while ( have_posts() ): ?>
                     <?php the_post();?>
@@ -11,13 +12,30 @@
                             <div>
                                 <h1> <?php  the_title(); ?> </h1>
                                 <p> <?php echo get_bloginfo('description') ?> </p>
+
+                                <?php if( has_nav_menu('social-menu') ): ?>
+                                <?php 
+                                wp_nav_menu(
+                                    array(
+                                            'container' => 'nav',
+                                            'container_id' => '',
+                                            'container_class' => '',
+                                            'theme_location' => 'social-menu',
+                                            'menu_class' => 'navigation-menu',
+                                            'item_spacing' => 'preserve'
+                                    )
+                                ); 
+                                ?>
+                            <?php endif; ?>
                             </div>
-                        </div>
-                    
+                        </div>                    
                 <?php endwhile; ?>
+
+
             <?php endif; ?>
 
         <?php else: ?>
+            <!-------------------------->
 
         <?php endif; ?>
 
