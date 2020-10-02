7152879379cb3ec7f874fcee4d528a04e49b34f4
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-10-02 23:40:49 +0200

    a

--- footer.php
+++ footer.php
@@ -23,22 +23,9 @@
     </body> 
     <?php wp_footer(); ?>
 
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-179494342-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-179494342-1');
-    </script>
-
-    <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W78T2MC');</script>
-<!-- End Google Tag Manager -->
+    <?php 
+        include get_parent_theme_file_path( 'google/analytics.php' );
+        include get_parent_theme_file_path( 'google/tags.php' );  
+    ?>
 
 </html>
--- header.php
+++ header.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
     <head>
+
+        <?php 
+            wp_head(); 
+        ?>
+        
         <meta charset="<?php bloginfo( 'charset' );?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" 
               content="ie=edge">
               
-        <?php 
-            wp_head(); 
-        ?>
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
         
     </head>
     <body>
-        <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W78T2MC"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+        <?php 
+            include get_parent_theme_file_path( 'google/tags_body.php' );
+        ?>
 
         <header>
             <?php if( has_nav_menu('header-menu') ): ?>
