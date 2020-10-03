6b072009ad03229ab3a1310d8501f49cb90a5780
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-10-03 02:20:26 +0200

    a

--- page-about-us.php
+++ page-about-us.php
@@ -2,7 +2,7 @@
     get_header(); 
 ?>    
         <main> 
-            
+        <?php include get_parent_theme_file_path('components/areas/cover.php'); ?>
         </main>
 <?php 
     get_footer();
