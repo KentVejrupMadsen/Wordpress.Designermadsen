6eaabf66ae2a14c187bee9be633f977899449445
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-10-03 17:42:21 +0200

    a

--- components/component-frontpage-post.php
+++ components/component-frontpage-post.php
@@ -1,40 +1,48 @@
+<?php 
+/* */
+
+$post_arg = array(
+    'posts_per_page' => 4, 
+    'orderby' => 'post_date',
+    'order' => 'desc',
+    'post_type' => 'post', 
+    'post_status' => 'publish'
+);
+
+$query = new WP_Query( $post_arg );
+
+if ( $query->have_posts() ) :
+?>  
 
 <div class="posts">
-            <h2> Posts </h2>
-                <div class="posts-container"> 
-                    <?php 
-                    $args = array(
-                        'posts_per_page' => 4, /* how many post you need to display */
-                        'orderby' => 'post_date',
-                        'order' => 'DESC',
-                        'post_type' => 'post', /* your post type name */
-                        'post_status' => 'publish'
-                    );
-                    $query = new WP_Query( $args );
-                    ?>  
+    <h2> 
+        Posts 
+    </h2>
+                
+    <div class="posts-container"> 
+        <?php 
+                while ( $query->have_posts() ) : $query->the_post();
+        ?>
+            <a class="post" href="<?php echo get_permalink(); ?>">
+                <div class="post-page">
 
-                    <?php 
-                    if ( $query->have_posts() ) :
-                        while ( $query->have_posts() ) : $query->the_post();
-                    ?>
-                    <a class="post" href="<?php echo get_permalink(); ?>">
-                        <div class="post-page"> 
-                            <?php if( has_post_thumbnail() ): ?>
-                                <?php the_post_thumbnail('designermadsen-thumb-medium_large'); ?>
-                            <?php else: // Figure something out ?>
+                    <?php if( has_post_thumbnail() ): ?>
+                                <?php the_post_thumbnail( 'designermadsen-thumb-medium_large' ); ?>
+                    <?php else: ?>
                                 
-                            <?php endif; ?>
+                    <?php endif; ?>
 
-                            <div class="text-container">
-                                <h3> <?php echo the_title(); ?> </h3>
-                                <p> <?php echo get_the_date(); ?> </p>
-                            </div>
-                        </div>
-                    </a>
-                    <?php
-                        endwhile;
-                    endif; ?>
-                    
-    <?php wp_reset_postdata(); ?>
+                    <div class="text-container">
+                        <h3> <?php echo the_title(); ?> </h3>
+                        <p> <?php echo get_the_date(); ?> </p>
+                    </div>
                 </div>
-            </div>
+            </a>
+        <?php
+                endwhile;
+        ?>
+    </div>
+    
+    <?php wp_reset_postdata(); ?>
+</div>
+<?php endif; ?>
--- page/frontpage.php
+++ page/frontpage.php
@@ -4,8 +4,10 @@
             <?php the_post();?>       
             <?php include get_parent_theme_file_path('components/areas/cover.php'); ?>                  
     <?php endwhile; ?>
+
+    <?php wp_reset_postdata(); ?>
 <?php endif; ?>
 
-
 <?php include get_parent_theme_file_path('components/component-frontpage-tasks.php'); ?> 
+<?php include get_parent_theme_file_path('components/component-frontpage-cases.php'); ?> 
 <?php include get_parent_theme_file_path('components/component-frontpage-post.php'); ?> 
