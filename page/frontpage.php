99cf003c73c11bcb7c21b314c90932c0919be965
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-10-03 02:13:23 +0200

    a

--- page/frontpage.php
+++ page/frontpage.php
@@ -1,35 +1,8 @@
 <!-------------------------->
 <?php if( have_posts() ): ?>
     <?php while ( have_posts() ): ?>
-            <?php the_post();?>
-                        
-            <div class="cover-image">
-                <?php //the_post_thumbnail(); 
-                ?> 
-
-                <img src="<?php echo get_the_post_thumbnail_url(); ?>" loading="lazy" />
-
-                <div>
-                    <h1> <?php  the_title(); ?> </h1>
-                    <p> <?php echo get_bloginfo('description') ?> </p>
-
-                    <?php if( has_nav_menu('social-menu') ): ?>
-                        <?php 
-                            wp_nav_menu(
-                                array(
-                                    'container' => 'nav',
-                                    'container_id' => '',
-                                    'container_class' => '',
-                                    'theme_location' => 'social-menu',
-                                    'menu_class' => 'navigation-menu',
-                                    'item_spacing' => 'preserve',
-                                    'walker' => new menu_social_walker()
-                                )
-                            ); 
-                        ?>
-                    <?php endif; ?>
-                </div>
-        </div>                    
+            <?php the_post();?>       
+            <?php include get_parent_theme_file_path('components/areas/cover.php'); ?>                  
     <?php endwhile; ?>
 <?php endif; ?>
 
